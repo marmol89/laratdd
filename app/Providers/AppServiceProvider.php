f42027c51da827949335be9b766dda39c13667e4
marmol89 <josito_marmol@hotmail.com>
2022-12-27 17:40:16 +0100

    Paso 43

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Sortable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\DB;
@@ -26,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Sortable::class, function ($app) {
+            return new Sortable(request()->url());
+        });
     }
 }
