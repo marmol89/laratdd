cd8e01806dd5fbb6115354572344b5247f5c64d1
marmol89 <josito_marmol@hotmail.com>
2022-12-27 17:13:31 +0100

    Paso 42

---
+++ app/Sortable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class Sortable
+{
+    protected $currentColumn;
+    protected $currentDirection;
+
+    public function setCurrentOrder($column, $direction)
+    {
+        $this->currentColumn = $column;
+        $this->currentDirection = $direction;
+    }
+
+    public function classes($column)
+    {
+        if($this->currentColumn == $column && $this->currentDirection == 'asc') {
+            return 'link-sortable link-sorted-up';
+        }
+
+        if ($this->currentColumn == $column && $this->currentDirection == 'desc') {
+            return 'link-sortable link-sorted-down';
+        }
+
+        return 'link-sortable';
+    }
+
+}
